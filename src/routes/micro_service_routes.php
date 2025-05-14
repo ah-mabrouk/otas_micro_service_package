036daa6744132e8f1c18e4b318213cf06c52f825
omar-ashraf-s <o.ashraf@solutionplus.net>
2025-05-14 13:12:47 +0300

    wip: create UpdateMicroserviceSecretController and endpoint feat: update microservice project secret and notify the other connections

--- src/routes/micro_service_routes.php
+++ src/routes/micro_service_routes.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Solutionplus\MicroService\Http\Controllers\MicroServiceMapController;
+use Solutionplus\MicroService\Http\Controllers\UpdateMicroserviceSecretController;
 
 Route::group([
     'prefix' => 'api',
-    'middleware' => [
-        'micro-service-establish-connection',
-    ]
 ], function () {
-    Route::apiResource('micro-services', MicroServiceMapController::class, ['only' => ['store']]);
+    Route::apiResource('micro-services', MicroServiceMapController::class, ['only' => ['store']])->middleware('micro-service-establish-connection');
+
+    Route::post('update-microservice-secret', UpdateMicroserviceSecretController::class)->name('update-microservice-secret')->middleware('micro-service');
 });
